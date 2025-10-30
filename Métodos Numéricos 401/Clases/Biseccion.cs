9c30274c87fb7d5d98c546ce9816b21f614a85ca
AlbertoV01 <albertovlzlar_@hotmail.com>
2025-10-30 14:34:36 -0600

    Remover librerias inecesarias

--- Métodos Numéricos 401/Clases/Biseccion.cs
+++ Métodos Numéricos 401/Clases/Biseccion.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Calculus;
-using NCalc;
+
 namespace Métodos_Numéricos_401
 {
 
